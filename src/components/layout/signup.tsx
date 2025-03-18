8bcd7f89f1558d933655293d7a7834a420baa134
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2025-03-18 17:12:26 +0100

    feat(initiated signup form): tanstack and zod

---
+++ src/components/layout/signup.tsx
@@ -0,0 +1,115 @@
+import { useForm } from "@tanstack/react-form";
+import { z } from "zod";
+import { Button } from "../ui/button";
+
+const signUpSchema = z.object({
+  login: z.string().min(2, {
+    message: "Login should be at least 2 characters",
+  }),
+  password: z.string().min(3, {
+    message: "Password should be at least 3 characters",
+  }),
+  strava_id: z.number().min(1, {
+    message: "Please provide a valid Strava Id",
+  }),
+  strava_secret: z.string().min(1, {
+    message: "Please provide a valid Strava secret",
+  }),
+});
+
+type Signup = z.infer<typeof signUpSchema>;
+
+export default function Signup() {
+  const form = useForm({
+    defaultValues: {
+      login: "",
+      password: "",
+      strava_id: 0, // TODO fix initial state
+      strava_secret: "",
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
+    },
+    validators: {
+      onChange: signUpSchema,
+    },
+  });
+
+  return (
+    <div className="flex justify-center my-[100px] mx-0">
+      <form.Field name="login">
+        {(field) => (
+          <>
+            <label htmlFor={field.name}>Login:</label>
+            <input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              type="text"
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            {field.state.meta.errors ? (
+              <em role="alert">{field.state.meta.errors.join(", ")}</em>
+            ) : null}
+          </>
+        )}
+      </form.Field>
+
+      <form.Field name="password">
+        {(field) => (
+          <>
+            <label htmlFor={field.name}>Password:</label>
+            <input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              type="text"
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            {field.state.meta.errors ? (
+              <em role="alert">{field.state.meta.errors.join(", ")}</em>
+            ) : null}
+          </>
+        )}
+      </form.Field>
+
+      <form.Field name="strava_id">
+        {(field) => (
+          <>
+            <label htmlFor={field.name}>Strava ID:</label>
+            <input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              type="string"
+              onChange={(e) => field.handleChange(e.target.valueAsNumber)}
+            />
+            {field.state.meta.errors ? (
+              <em role="alert">{field.state.meta.errors.join(", ")}</em>
+            ) : null}
+          </>
+        )}
+      </form.Field>
+
+      <form.Field name="strava_secret">
+        {(field) => (
+          <>
+            <label htmlFor={field.name}>Strava secret:</label>
+            <input
+              id={field.name}
+              name={field.name}
+              value={field.state.value}
+              type="text"
+              onChange={(e) => field.handleChange(e.target.value)}
+            />
+            {field.state.meta.errors ? (
+              <em role="alert">{field.state.meta.errors.join(", ")}</em>
+            ) : null}
+          </>
+        )}
+      </form.Field>
+
+      <Button onClick={form.handleSubmit}>Sign up</Button>
+    </div>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import Signup from "@/components/layout/signup";
 
 export default function Home() {
-  const { data, error, isLoading } = useQuery({
-    queryKey: ["test"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3000/api/auth");
-      return await response.json();
-    },
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>An error occurred: {error.message}</p>;
   return (
     <div>
-      <p>Response from API: {JSON.stringify(data)}</p>
+      <Signup />
     </div>
   );
 }
