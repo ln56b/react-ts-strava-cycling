a7614d6ffbb8a5e0c25cad1f23ed93ae2a6ec0f4
Hélène Baudrand <baudrand.helene@gmail.com>
2025-06-30 10:46:32 +0200

    feat(added strava logo): powered by stack

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,7 +5,7 @@ import { Switch } from '../ui/switch';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	const { logout } = useAuth();
-	const { toggleTheme } = useTheme();
+	const { toggleTheme, theme } = useTheme();
 
 	return (
 		<div className="p-4">
@@ -16,6 +16,17 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 				</Button>
 			</div>
 			{children}
+			<div className="absolute bottom-4 right-4	">
+				<img
+					width={100}
+					src={
+						theme === 'dark'
+							? 'src/assets/strava-white.svg'
+							: 'src/assets/strava-black.svg'
+					}
+					alt="powered by Strava"
+				></img>
+			</div>
 		</div>
 	);
 }
