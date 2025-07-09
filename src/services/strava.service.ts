c039c879b197840d6a9905fc6f259ac9389cf944
Hélène BAUDRAND <Helene@MacBook-Pro-de-apixis.local>
2025-07-09 16:29:47 +0200

    refactor(initial api call): fetch all activities at once
    no further api call - rate limit

--- src/services/strava.service.ts
+++ src/services/strava.service.ts
@@ -1,119 +1,102 @@
-import { Activity } from '@/interfaces/strava';
-import { useActivitiesStore } from '@/stores/activitiesStore';
-import { environment } from '@/environments/environment';
-import { toast } from 'sonner';
+import { Activity } from "@/interfaces/strava";
+import { useActivitiesStore } from "@/stores/activitiesStore";
+import { environment } from "@/environments/environment";
+import { toast } from "sonner";
 
 const apiUrl = environment.apiUrl;
 
 export const loadActivities = async (): Promise<Activity[] | undefined> => {
-	const tokenValid = await checkStravaTokensValidity();
+  const tokenValid = await checkStravaTokensValidity();
+  if (!tokenValid) await refreshStravaTokens();
 
-	if (!tokenValid) {
-		await refreshStravaTokens();
-	}
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) {
+    toast.error("You are not identified");
+    return;
+  }
 
-	const accessToken = localStorage.getItem('accessToken');
-	if (!accessToken) {
-		toast.error('You are not identified');
-		return;
-	}
+  let page = 1;
+  const per_page = 100;
+  const allActivities: Activity[] = [];
 
-	let page = 1;
-	const per_page = 10
-	const apiRequests = [];
-	const apiResponses = [];
+  try {
+    while (true) {
+      const queryParams = new URLSearchParams({
+        page: page.toString(),
+        per_page: per_page.toString(),
+      });
+      const url = `https://www.strava.com/api/v3/athlete/activities?${queryParams.toString()}`;
 
-	const queryParams = new URLSearchParams({
-		page: page.toString(),
-		per_page: per_page.toString()
-	});
+      const res = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data: Activity[] = await res.json();
 
-	const url = `https://www.strava.com/api/v3/athlete/activities?${queryParams.toString()}`;
+      if (!Array.isArray(data)) {
+        throw new Error("Invalid response from Strava API");
+      }
 
-	try {
-		const firstResponse = await fetch(`${url}`, {
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		});
-	
-		const firstRes = await firstResponse.json();
-		apiResponses.push(...firstRes);
-	
-		const nextPage = firstRes.length > per_page -1;
-	
-		if (nextPage) {
-			queryParams.set('page', page.toString());
-		}
-	
-		while (nextPage) {
-			page++;
-			console.log('fetching next page', page);
-			apiRequests.push(fetch(`${url}`, {
-				headers: {
-					Authorization: `Bearer ${accessToken}`,
-				},
-			}).then(res => res.json()).catch(err => {
-				toast.error(err.message);
-			}) )
-		}
-	
-		const restData = await Promise.all(apiRequests);
-		const res = [...apiResponses, ...restData];
-	
-		if (res) {
-			useActivitiesStore.setState({ activities: res });
-			return res;
-		}
-	} catch (err: any) {
-		toast.error(err.message);
-	}
+      allActivities.push(...data);
+
+      if (data.length < per_page) break;
+
+      page++;
+    }
+
+    useActivitiesStore.setState({ activities: allActivities });
+    return allActivities;
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+  } catch (err: any) {
+    toast.error(err.message);
+  }
 };
 
 const checkStravaTokensValidity = async () => {
-	const tokenExpiresAt = localStorage.getItem('tokenExpiresAt');
+  const tokenExpiresAt = localStorage.getItem("tokenExpiresAt");
 
-	if (!tokenExpiresAt) return false;
-	const now = new Date();
-	const tokenExpiresAtDate = new Date(Number(tokenExpiresAt) * 1000);
-	if (tokenExpiresAtDate < now) {
-		return false;
-	}
-	return true;
+  if (!tokenExpiresAt) return false;
+  const now = new Date();
+  const tokenExpiresAtDate = new Date(Number(tokenExpiresAt) * 1000);
+  if (tokenExpiresAtDate < now) {
+    return false;
+  }
+  return true;
 };
 
 const refreshStravaTokens = async () => {
-	const accessToken = localStorage.getItem('accessToken');
-	if (!accessToken) {
-		toast.error('You are not identified');
-		return;
-	}
-	const refreshToken = localStorage.getItem('refreshToken');
-	if (!refreshToken) {
-		toast.error('You are not identified');
-		return;
-	}
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) {
+    toast.error("You are not identified");
+    return;
+  }
+  const refreshToken = localStorage.getItem("refreshToken");
+  if (!refreshToken) {
+    toast.error("You are not identified");
+    return;
+  }
 
-	try {
-		const response = await fetch(`${apiUrl}/users/strava-refresh-tokens`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `Bearer ${accessToken}`,
-			},
-			body: JSON.stringify({ refreshToken }),
-		});
+  try {
+    const response = await fetch(`${apiUrl}/users/strava-refresh-tokens`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ refreshToken }),
+    });
 
-		const res = await response.json();
-		if (res) {
-			const { access_token, refresh_token, expires_at } = res;
-			localStorage.setItem('accessToken', access_token);
-			localStorage.setItem('refreshToken', refresh_token);
-			localStorage.setItem('tokenExpiresAt', expires_at);
-			return res;
-		}
-		throw new Error(res.message);
-	} catch (err) {
-		console.log('Error', err);
-	}
+    const res = await response.json();
+    if (res) {
+      const { access_token, refresh_token, expires_at } = res;
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("refreshToken", refresh_token);
+      localStorage.setItem("tokenExpiresAt", expires_at);
+      return res;
+    }
+    throw new Error(res.message);
+  } catch (err) {
+    console.log("Error", err);
+  }
 };
