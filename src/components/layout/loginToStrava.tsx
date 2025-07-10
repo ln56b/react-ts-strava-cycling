5a352c480b998bd2be5ee217db29c38189205d3e
Hélène Baudrand <baudrand.helene@gmail.com>
2025-07-10 15:42:44 +0200

    refactor(strava button connect with): use strava brand guidelines

--- src/components/layout/loginToStrava.tsx
+++ src/components/layout/loginToStrava.tsx
@@ -1,7 +1,10 @@
 import { authorizeStrava } from '@/services/users.service';
 import { Button } from '../ui/button';
+import { useTheme } from '@/providers/themeProvider';
 
 export default function LoginToStrava() {
+	const { theme } = useTheme();
+
 	const connectToStrava = () => {
 		const stravaId = localStorage.getItem('stravaId');
 		if (!stravaId) {
@@ -11,8 +14,18 @@ export default function LoginToStrava() {
 	};
 
 	return (
-		<div className="flex justify-center items-center my-[100px] mx-0 ">
-			<Button onClick={connectToStrava}>Connect To Strava</Button>
+		<div className="flex justify-center items-center my-[100px] mx-0">
+			<Button onClick={connectToStrava} variant="ghost">
+				<img
+					className="w-60"
+					src={
+						theme === 'dark'
+							? 'src/assets/btn-strava-connect-white.png'
+							: 'src/assets/btn-strava-connect-orange.png'
+					}
+					alt="Button Connect With Strava"
+				/>
+			</Button>
 		</div>
 	);
 }
