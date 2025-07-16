4bc855278bf67f926de2247205598d8323deefda
Hélène Baudrand <baudrand.helene@gmail.com>
2025-07-16 17:27:23 +0200

    feat(new user store): handle preferred theme and lastLogin

---
+++ src/stores/userStore.ts
@@ -0,0 +1,31 @@
+import { Theme } from '@/interfaces/project';
+import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
+export interface UserState {
+	theme: Theme;
+	lastLogin: string;
+	loading: boolean;
+	error: string | null;
+}
+
+export interface UserActions {
+	setTheme: (theme: Theme) => void;
+}
+
+export const userInitialState: UserState = {
+	theme: Theme.Dark,
+	lastLogin: '',
+	loading: false,
+	error: null,
+};
+
+const createUserStore = (
+	set: (partial: Partial<UserState & UserActions>) => void
+) => ({
+	setTheme: (theme: Theme) => set({ theme }),
+});
+
+export const useUserStore = create<UserState & UserActions>()(
+	devtools((set) => createUserStore(set), { name: 'UserStore' })
+);
